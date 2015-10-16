7bb1f3dd4af7bd950be2585bc2293f6a589063a7
Guoqiang GUAN <gqguan@hotmail.com>
2015-10-17 01:24:52 +0800

    Edit .gitignore to exclude the files in debug folder

--- Dynamic dialysis/main.F90
+++ Dynamic dialysis/main.F90
@@ -1,4 +1,42 @@
-program DD15
+program DD1S
+
+! Nomenclature:
+! DD means the dynamic dialysis
+! 1  means the 1-D model
+! S  means the steady state
+
+! Problem description:
+! The steady-state simulation of heat and mass transfer from a tubular membrane
+! module will be performed for an extensive application of dynamic dialysis, 
+! where a tubular dialysis membrane divides the champer into tube side and 
+! shell side respectively. The models could be found in the document of 
+! "20151014 Model of dynamic dialysis".
+
+! Application example:
+! For a typical application, where the ethanol and water solution is fed 
+! in the tube side and the water is fed in the shell side, 
+! the tube-side solute will permeate into the shell side through the membrane
+! due to the concentration gradient. The shell-side solvent will also reversely
+! permeate into the tube side. 
+! The mass concentration of each component in the influents of both sides, 
+! together with the velocities and Reynolds number, should be known in advance.
+! A constant pressure gradient along the flowing path was also assumed to
+! simplify the equations of the momentum balance.
+
+! Program structure:
+! main.f90 
+! |-- prepare.f90    (modules of collecting the variables and parameters)
+! |-- solver.f90     (modules of modelling equations and ode solver)
+! |-- supplement.f90 (modules of subroutines and customized functions)
+
+! License:
+! This code is distributed under the GNU LGPL license.
+
+! Authors:
+! Guoqiang GUAN Dr. (gqguan@scut.edu.cn)
+
+! Revision log:
+! Refer to https://github.com/gqguan/dialysis
 
 implicit none
 
